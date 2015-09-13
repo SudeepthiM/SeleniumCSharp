4d735a8225925b038e61f7976c4b482607344d39
Sudeepthi <sudeepthi.mogalapalli@yahoo.co.uk>
2015-09-13 20:41:25 +0100

    intial commit

---
+++ BBCWeather/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using System.Diagnostics;
+using System.Threading;
+
+namespace BBCWeather
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void Initalise()
+        {
+            driver = new FirefoxDriver();
+            System.Environment.SetEnvironmentVariable("restart.browser.each.scenario", "false");
+        }
+        
+        public void GotoURL()
+        {
+            driver.Url = "http://www.bbc.co.uk/";
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2000));
+            driver.FindElement(By.XPath(".//*[@id='orb-nav-links']/ul/li[3]/a")).Click();
+            String CurrentURL = driver.Url;
+            Assert.AreEqual("http://www.bbc.co.uk/weather/", CurrentURL);
+            driver.FindElement(By.XPath(".//*[@id='locator-form-search']")).SendKeys("Reading, Reading");
+            driver.FindElement(By.XPath(".//*[@id='locator-form-submit']")).Click();
+            String title = driver.Title;
+            Debug.WriteLine("Weather Reading is opened");
+            Assert.AreEqual("BBC Weather - Reading", title);
+         }
+
+        [TestMethod]
+        public void FurtherAhead()
+        {
+            GotoURL();
+            //driver.FindElement(By.XPath(".//*[@id='extended-forecast']/span[2]")).Click();
+            //Thread.Sleep(2000);
+            //driver.FindElement(By.XPath(".//*[@id='basic-forecast']/span[2]")).Click();
+        }
+        
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ NUnit/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        IWebDriver _driver;
+        String currentUrl;
+
+        [SetUp]
+        public void GotoBbcWeather()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Url = "http://www.bbc.co.uk/";
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2000));
+            _driver.FindElement(By.XPath(".//*[@id='orb-nav-links']/ul/li[3]/a")).Click();
+            currentUrl = _driver.Url;
+            _driver.FindElement(By.XPath(".//*[@id='locator-form-search']")).SendKeys("Reading, Reading");
+            _driver.FindElement(By.XPath(".//*[@id='locator-form-submit']")).Click();
+            
+        }
+
+        [Test]
+        public void CheckWethereOnReadingOrNot()
+        {
+            
+            Assert.AreEqual("http://www.bbc.co.uk/weather/", currentUrl);
+            String title = _driver.Title;
+            Debug.WriteLine("Weather Reading is opened");
+            Assert.AreEqual("BBC Weather - Reading", title);
+        }
+
+        [Test]
+        public void FurtherAhead()
+        {
+            
+            _driver.FindElement(By.XPath(".//*[@id='extended-forecast']/span[2]")).Click();
+            Thread.Sleep(2000);
+            _driver.FindElement(By.XPath(".//*[@id='basic-forecast']/span[2]")).Click();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Close();
+        }
+    }
+}
